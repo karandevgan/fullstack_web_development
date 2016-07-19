5d5b3d660bfa0fa934feaf1b83ea5166b54bdb69
karanpaldewgun <karanpal_dewgun@persistent.co.in>
2016-07-19 21:38:17 +0530

    Fixed: connect in Gruntfile.js

--- Course3/Week2/conFusion/Gruntfile.js
+++ Course3/Week2/conFusion/Gruntfile.js
@@ -136,17 +136,19 @@ module.exports = function (grunt) {
         connect: {
             options: {
                 port: 9000,
-                //Change this to 0.0.0.0 to access server from outside
+                // Change this to '0.0.0.0' to access the server from outside.
                 hostname: 'localhost',
                 livereload: 35729
             },
             dist: {
-                open: true,
-                base: {
-                    path: 'dist',
-                    options: {
-                        index: 'menu.html',
-                        maxAge: 300000
+                options: {
+                    open: true,
+                    base: {
+                        path: 'dist',
+                        options: {
+                            index: 'menu.html',
+                            maxAge: 300000
+                        }
                     }
                 }
             }
