30b0b8a8380d9393069d3a2ba964537617d9e81e
karanpaldewgun <karanpal_dewgun@persistent.co.in>
2016-07-31 02:52:13 +0530

    Started writing test cases with Karma and Jasmine

--- Course3/Week4/conFusion/app/scripts/services.js
+++ Course3/Week4/conFusion/app/scripts/services.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('confusionApp')
-    .constant("baseURL", "http://localhost:3002/")
+    .constant("baseURL", "http://localhost:3000/")
     .factory('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
         var menuFactory = {};
